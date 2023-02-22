bc49be2af3867b4965052821cda4395034e74ba1
Aslihan Hird <aslihan.hird@gmail.com>
2023-02-22 14:37:36 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+puts 'Destroying previous restaurants...'
+5.times do
+  restaurant = Restaurant.create({
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: %w[chinese italian japanese french belgian].sample
+  })
+  puts "Creating restaurant with id: #{restaurant.id}..."
+end
